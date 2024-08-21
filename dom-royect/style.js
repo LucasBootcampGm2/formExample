18bcdc9e02cd5560e92e53ad0b5ec0d56e26559d
Lucas <lcirilloberardi@alumno.huergo.edu.ar>
2024-08-20 21:40:46 -0300

    feat(dom-proyect): create a filter input-search

--- dom-royect/style.js
+++ dom-royect/style.js
@@ -6,3 +6,19 @@ filterInputs.forEach(function(button){
         inputSearch.value = button.value
     })
 })
+
+let cards = document.querySelectorAll('.card')
+let types = document.querySelectorAll('.card-type-p')
+console.log(cards)
+console.log(types)
+inputSearch.addEventListener('input', function(){
+    types.forEach(function(type,idx){
+        if (!(type.innerHTML === inputSearch.value)){
+            cards[idx].classList.add('hidden')
+        } else {
+            cards[idx].classList.remove('hidden')
+        }
+            
+    })
+    
+})
