15f65c91f378dacbde66e064f0f0f3a781185115
Lucas <lcirilloberardi@alumno.huergo.edu.ar>
2024-08-21 22:09:10 -0300

    feat(style.js): make sport filter button

--- dom-royect/style.js
+++ dom-royect/style.js
@@ -1,24 +1,42 @@
 let inputSearch = document.getElementById('input-search')
 let filterInputs = document.querySelectorAll('.filter-inputs')
 
+let cards = document.querySelectorAll('.card')
+let types = document.querySelectorAll('.card-type-p')
+
 filterInputs.forEach(function(button){
     button.addEventListener('click', function(){
         inputSearch.value = button.value
+        types.forEach(function(type,idx){
+            if (!(types[idx].innerHTML === inputSearch.value)){
+                cards[idx].classList.add('hidden')
+            } 
+            else {
+                cards[idx].classList.remove('hidden')
+            }
+                
+        })    
     })
 })
 
-let cards = document.querySelectorAll('.card')
-let types = document.querySelectorAll('.card-type-p')
-console.log(cards)
-console.log(types)
+
 inputSearch.addEventListener('input', function(){
     types.forEach(function(type,idx){
-        if (!(type.innerHTML === inputSearch.value)){
-            cards[idx].classList.add('hidden')
-        } else {
+        if (type.innerHTML === inputSearch.value){
             cards[idx].classList.remove('hidden')
+        } 
+        else {
+            cards[idx].classList.add('hidden')
         }
             
     })
-    
+})
+
+
+console.log(inputSearch)
+
+inputSearch.addEventListener('load', function(){
+    if (inputSearch.value === 'null') {
+        cards[idx].classList.remove('hidden')
+    } 
 })
